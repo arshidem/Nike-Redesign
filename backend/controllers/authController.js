d9a65ec7cee82607131184ee18749a70ef301634
arshidem <arshidem00@gmail.com>
2025-09-25 18:04:17 +0530

    Initial commit

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -13,7 +13,7 @@ const sendOTP = async (email, otp) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: false,
+    secure: true,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
